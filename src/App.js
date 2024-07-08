47b4ce810b88a8cc06a6f99bf5018a04c76be2e7
Reed Hazen <redhazen@gmail.cocm>
2024-07-08 14:42:29 -0700

    starting to add components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
